1915a27d0ab6d8b907d50122207c44ec156858ea
CoreyTodd7 <mendezjohnoy.jm@gmail.com>
2019-10-25 13:49:12 -0500

    FixProblems

--- IQuad2/ViewModels/AppointmentViewModel.cs
+++ IQuad2/ViewModels/AppointmentViewModel.cs
@@ -6,10 +6,22 @@ using System.Web;
 
 namespace IQuad2.ViewModels
 {
-    public class AppointmentViewModel
+    public class AppointmentViewModel 
     {
         public IEnumerable<User> Doctor { get; set; } 
         public Appointment appointment { get; set; }
+        public IEnumerable<string> Id { get; set; }
+
+        public AppointmentViewModel()
+        {
+
+        }
+       /* public AppointmentViewModel(string number)
+        {
+            var user = new User();
+            Id = number;
+        }*/
+
         
     }
 }
