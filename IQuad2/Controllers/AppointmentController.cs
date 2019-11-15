6a53e47fb98377b94f7ecea68ab3797f68ebdf31
CoreyTodd7 <mendezjohnoy.jm@gmail.com>
2019-11-15 15:25:40 -0500

    new changes

--- IQuad2/Controllers/AppointmentController.cs
+++ IQuad2/Controllers/AppointmentController.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNet.Identity;
-using System.Linq;
-using System.Security.Principal;
-using System.Web;
+﻿using Microsoft.AspNet.Identity;
 using System.Web.Mvc;
 using IQuad2.Models;
 using IQuad2.ViewModels;
-using IQuad2.Services;
 
 namespace IQuad2.Controllers
 {
-   
+
     public class AppointmentController : Controller
     {
         // GET: Appointment
